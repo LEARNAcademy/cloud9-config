73f510b2f3c4642acc801d8b06459a3595e9cef6
Matt <matt+github@notch8.com>
2019-04-22 17:27:16 -0700

    add yarn to apt installs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install postgresql postgresql-contrib libpq-dev
+apt-get -y install postgresql postgresql-contrib libpq-dev yarn
 sudo -u postgres createuser -s ubuntu
 
 sudo -i -u ubuntu bash << EOF

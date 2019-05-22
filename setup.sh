7c870ccde9dd795acf3bd229528bbc7a5c261eab
winescout <matt@notch8.com>
2019-05-22 09:58:28 -0700

    remove nvm install, its already on ubuntu

--- setup.sh
+++ setup.sh
@@ -14,9 +14,6 @@ gem install bundler
 gem install rails -v 5.2.3
 
 # Now Node
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh
-bash install_nvm.sh
-source ~/.profile
 nvm install 10.15.3
 nvm alias default 10.15.3
 

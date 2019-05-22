52850a05d3d733bc12b447ddde1b081882da1af9
winescout <matt@notch8.com>
2019-05-22 10:58:48 -0700

    switch to root for first part of install

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Get a current version of yarn
-echo "Command: curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo su bash << EOF
+echo "Command: curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg |  apt-key add -"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 echo "Command: apt-get update"
-sudo apt-get update
+apt-get update
 
 echo "Command: apt-get -y install postgresql postgresql-contrib libpq-dev yarn"
-sudo apt-get -y install postgresql postgresql-contrib libpq-dev yarn
+apt-get -y install postgresql postgresql-contrib libpq-dev yarn
 
 echo "Command: sudo -u postgres createuser -s ubuntu"
 sudo -u postgres createuser -s ubuntu
 
 echo "Command: sudo apt autoremove"
-sudo apt autoremove
+apt autoremove
+
+EOF
 
 echo "Command: gem install bundler"
 gem install bundler

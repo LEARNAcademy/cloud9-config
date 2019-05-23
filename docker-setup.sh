5ea5be382b1505db81b71f4085d93dc10253353d
winescout <matt@notch8.com>
2019-05-22 20:58:48 -0700

    adds chmod for docker-conpose install

--- docker-setup.sh
+++ docker-setup.sh
@@ -8,5 +8,6 @@ sudo usermod -aG docker ubuntu
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo usermod -aG docker $USER

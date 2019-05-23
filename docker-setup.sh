0b7a49ac7a4e4bcfb296f7fb0d50c9616715973f
winescout <matt@notch8.com>
2019-05-22 19:52:26 -0700

    removes some echo from main setup, adds a docker setup script

---
+++ docker-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker ubuntu
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo usermod -aG docker $USER
--- setup.sh
+++ setup.sh
@@ -2,43 +2,23 @@
 
 # Get a current version of yarn
 sudo -i -u root bash << EOF
-echo "Command: curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg |  apt-key add -"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-
-echo "Command: apt-get update"
 apt-get update
-
-echo "Command: apt-get -y install postgresql postgresql-contrib libpq-dev yarn"
 apt-get -y install postgresql postgresql-contrib libpq-dev yarn
-
-echo "Command: sudo -u postgres createuser -s ubuntu"
 sudo -u postgres createuser -s ubuntu
 
-echo "Command: sudo apt autoremove"
 apt autoremove
 
 EOF
 
-echo "Command: gem install bundler"
 gem install bundler
-
-echo "Command: gem install rails -v 5.2.3"
 gem install rails -v 5.2.3
 
 # Now Node
-echo "Command: nvm install 10.15.3"
 nvm install 10.15.3
-
-echo "Command: nvm alias default 10.15.3"
 nvm alias default 10.15.3
 
 # React
-
-echo "Command: yarn global add react@16.8.6"
 npm install -g react@16.8.6
-
-echo "Cpommand: yarn global add create-react-app@3.0.1"
 npm install -g create-react-app@3.0.1
-
